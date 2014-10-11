50195f840be3c8076a82620b72193ac119899c93
GhostCoinOfficial <developer@ghostcoin.info>
2014-10-11 17:08:15 -0400

    Fix, removed trailing comma

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"ghostcoin.info", "ghostcoin.info"},
+    {"ghostcoin.info", "ghostcoin.info"}
 };
 
 void ThreadDNSAddressSeed(void* parg)
